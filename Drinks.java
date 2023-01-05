415c624fd4d512ffa4be2921e2b0ab4ef013a2c0
jessie1sharon <83138460+jessie1sharon@users.noreply.github.com>
2023-01-05 17:30:40 +0200

    Update Drinks.java

--- Drinks.java
+++ Drinks.java
@@ -1,10 +1,4 @@
 package Ordering;
-//Group number 3
-//Margarita kaplan 321775579
-//Jessica Llanos 327083184
-//Yarden Phahima 318647211
-//Shahar Ganani 208983247
-
 
 public class Drinks extends Food implements Orderable {
 	
